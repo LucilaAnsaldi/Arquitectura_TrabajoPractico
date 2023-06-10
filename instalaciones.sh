e7ea621704d186bcd3704de36068816117da8098
Olivia Luciano <112518065+olivialuciano2003@users.noreply.github.com>
2023-06-10 01:45:03 -0300

    ahí arreglé el script porque faltaba código para que instale docker y docker compose instalaciones.sh

--- instalaciones.sh
+++ instalaciones.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 if ! [ -x "$(command -v docker)" ]; then
-    # Install Docker
     echo "Installing Docker..."
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
     sudo usermod -aG docker $USER
@@ -15,6 +16,8 @@ fi
 
 if ! [ -x "$(command -v docker-compose)" ]; then
     echo "Installing Docker Compose..."
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
     sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
     echo "Docker Compose installed successfully."
