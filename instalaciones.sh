95e9310de215dec593ea587744ac2537c20a624a
LucilaAnsaldi <110502452+LucilaAnsaldi@users.noreply.github.com>
2023-06-09 21:02:33 -0300

    Add instalaciones.sh

---
+++ instalaciones.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if ! [ -x "$(command -v docker)" ]; then
+    # Install Docker
+    echo "Installing Docker..."
+    curl -fsSL https://get.docker.com -o get-docker.sh
+    sudo sh get-docker.sh
+    sudo usermod -aG docker $USER
+    sudo systemctl enable docker
+    sudo systemctl start docker
+    echo "Docker installed successfully."
+else
+    echo "Docker is already installed."
+fi
+
+if ! [ -x "$(command -v docker-compose)" ]; then
+    echo "Installing Docker Compose..."
+    sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    echo "Docker Compose installed successfully."
+else
+    echo "Docker Compose is already installed."
+fi
